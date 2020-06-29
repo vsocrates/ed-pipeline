b7953d35ab441f86c55ccca98024bf650047979b
Patrick Young <patrick.young@yale.edu>
2020-06-29 15:42:20 -0400

    retool for gitlab

--- scripts/build_conda_env.sh
+++ scripts/build_conda_env.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-mkdir -p ~/.ssh
-chmod 700 ~/.ssh
-ssh-keygen -F github1vp.ynhh.org 2>/dev/null 1>/dev/null
-if [ $? -eq 0 ];
-then
-  echo “github host is already known”
-else
-  ssh-keyscan -t rsa -T 10 github1vp.ynhh.org >> ~/.ssh/known_hosts
-fi
-
 export PIP_SRC="~/.conda/envs/py3env/"
 source activate py3env
 if [ $? -eq 0 ];
