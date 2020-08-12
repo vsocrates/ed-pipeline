d0a94279717c85a29b85134d0ab82f500ef73a24
rockdoctor <34164718+rockdoctor@users.noreply.github.com>
2020-08-12 11:19:27 -0400

    remove zip and chmod

--- build.py
+++ build.py
@@ -1,6 +1,4 @@
 import os
 
 os.system("rm ./*.zip")
-os.chmod("scripts/build_conda_env.sh", 0o755)
 os.system("scripts/build_conda_env.sh")
-
--- driver.py
+++ driver.py
@@ -1,28 +1,19 @@
 from pyspark.sql import SparkSession
 import os
-from distutils import util
 
 
 # Set PYSPARK_PYTHON
 os.environ["PYSPARK_PYTHON"] = "./temp_envs/py3env/bin/python"
 
-# Zip Modules
-os.system("zip modules.zip -r modules/*.py")
-
 spark = SparkSession\
     .builder\
     .getOrCreate()
 
-spark.sparkContext.addPyFile("/home/cdsw/modules.zip")
-
 try:
-    from cchlib import copymerge
+    from cchlib import schematic
 except ImportError:
     raise ImportError('Error importing cchlib module. Run build.py first')
 
 # Import and instantiate your code from modules here
 
 spark.stop()
-
-# Remove Module Zip
-os.system("rm modules.zip")
