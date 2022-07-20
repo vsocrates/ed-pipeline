d8e3b53e4cf1c3a0116f714a4ab5b2f5f14108ba
Vimig Socrates <vimig.socrates@gmail.com>
2022-07-20 16:36:59 -0400

    update test cli

--- src/ed_pipeline/cli.py
+++ src/ed_pipeline/cli.py
@@ -9,13 +9,14 @@ from src.ed_pipeline.utils import helper_variables, helpful_functions
 import typer
 from typer import Argument
 
+from src.ed_pipeline.modules import demos
 
 def main(n: int = Argument(..., min=0, help="The input n of fact(n)")) -> None:
     """Compute factorial of a given input."""
     colorama.init(autoreset=True, strip=False)
 
     spark = helpful_functions.get_spark_session("test", SparkConf())
-
+    main_df, rand_df = demos.demographic_pull(spark, "visit_occurrence_id", age = [30,50], rand_sample_size= 1000)
 
 def entry_point() -> None:
     typer.run(main)
